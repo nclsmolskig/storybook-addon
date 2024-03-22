a00a4138c44ad41678e4d928985ff2d5ffd20afe
Alexandre Figueiredo <ale.fig1701@gmail.com>
2024-03-22 16:32:46 -0300

    Add path prop inside parameter object

--- src/Panel/Panel.tsx
+++ src/Panel/Panel.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React from 'react';
+import { useParameter } from '@storybook/api';
 import { addons } from "@storybook/manager-api";
 import { Channel } from "diagnostics_channel";
+
+const PARAM_KEY = 'componentPath';
 
 interface PanelProps {
   active: boolean;
 }
 
-export const Panel: React.FC<PanelProps> = () => {
+export const Panel: React.FC<PanelProps> = ({ active }) => {
   const channel: Channel = addons.getChannel();
+  const componentPath = useParameter<string>(PARAM_KEY, null);
 
-  return channel ? <div>{channel.data.storyRendered}</div> : null;
+  if (active && componentPath) {
+    return <div>Component Path: {componentPath} {channel ? <div>Channel: {channel.data.storyRendered}</div> : null}</div>;
+  }
+
+  return null;
 };
