1b23457fa86a60299a7d604eb68a102c5f745b3f
Gabriel Smolski <gsmolski@nclcorp.com>
2024-03-28 10:27:55 -0300

    [VDS-250] fixing render on different stories

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,45 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { API, useParameter } from "@storybook/manager-api";
-import { AddonPanel } from "@storybook/components";
-import { getGraphRender } from "../scripts/utils"
+import { AddonPanel, P } from "@storybook/components";
+import { getGraphRender } from "../scripts/utils";
 import { PARAM_KEY } from "../scripts/constants";
 import { DepGraphParams } from "../scripts/types";
 import { depGraphDefaultParams } from "../scripts/data";
 
 //TODO this section needs to be isolated with the UI render component
-import mermaid from 'mermaid';
+import mermaid from "mermaid";
 mermaid.initialize({ startOnLoad: false });
 
 interface PanelProps {
-  active: boolean
-  api: API
+  active: boolean;
+  api: API;
 }
 
 function Panel({ active, api }: PanelProps) {
-  const currentStoryData = api.getCurrentStoryData()
-  const params = useParameter<Partial<DepGraphParams>>(PARAM_KEY, depGraphDefaultParams);
-  const normalizedParams = Object.assign(params, depGraphDefaultParams)
-  const render = getGraphRender(normalizedParams, currentStoryData)
+  const currentStoryData = api.getCurrentStoryData();
+  const params = useParameter<Partial<DepGraphParams>>(
+    PARAM_KEY,
+    depGraphDefaultParams,
+  );
+  const normalizedParams = Object.assign(params, depGraphDefaultParams);
+  const render = getGraphRender(normalizedParams, currentStoryData);
 
-  useEffect(() => { //TODO this section needs to be isolated with the UI render component
-    mermaid.run()
-      .then(() => console.log('Mermaid Render Done'))
-      .catch(() => console.error('Mermaid Render Failed'));
-  }, [render]);
+  const mermaidRef = useRef(null);
+
+  useEffect(() => {
+    //TODO this section needs to be isolated with the UI render component
+    mermaid
+      .run()
+      .then(() => {
+        mermaidRef.current &&
+          mermaidRef.current.removeAttribute("data-processed");
+      })
+      .catch(() => console.error("Mermaid Render Failed"));
+  }, [render.value]);
 
   return (
     <AddonPanel active={active}>
-      <div style={{ display: "flex", flexDirection: "column", gap: '8px'  }}>
+      <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
         <h1> NCLH Dependency Graph Addon</h1>
-        <h2> Story ID: { currentStoryData.id }</h2>
-        <h2> Story Import Path: { currentStoryData.importPath }</h2>
+        <h2> Story ID: {currentStoryData.id}</h2>
+        <h2> Story Import Path: {currentStoryData.importPath}</h2>
         {/*TODO Create a component for graph displaying and navigation. Consider conditional components based on UI_Type*/}
-        <div style={{ background: "white", color: "black", padding: '16px'  }}>
+        <div style={{ background: "white", color: "black", padding: "16px" }}>
           <h3> Graph: </h3>
-          { render.error
-            ? <p> { render.value } </p>
-            : <p className={'mermaid'}> { render.value } </p>
-          }
+          {render.error ? (
+            <p> {render.value} </p>
+          ) : (
+            <p className={"mermaid"} ref={mermaidRef}>
+              {render.value}
+            </p>
+          )}
         </div>
       </div>
     </AddonPanel>
